75b3072e38fbd70ee7b775cf8477d50df3acdb61
Dynatic <yyhyoo0723@naver.com>
2017-08-31 00:29:37 +0900

    메인 액티비티 잘못된 머지 수정.

--- app/src/main/java/dynatic/traveldiary/main/MainActivity.java
+++ app/src/main/java/dynatic/traveldiary/main/MainActivity.java
@@ -2,12 +2,17 @@ package dynatic.traveldiary.main;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import dynatic.traveldiary.R;
 import dynatic.traveldiary.base.BaseActivity;
+import kyora.itineraryalgorithms.ItineraryAlgorithms;
 
 public class MainActivity extends BaseActivity {
+
+    private Button btnCheck;
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,17 @@ public class MainActivity extends BaseActivity {
 
         View view = new TextView(this);
         view.setOnClickListener(v -> {
+        });
+
+        btnCheck = (Button) findViewById(R.id.btnCheck);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                ItineraryAlgorithms itr = new ItineraryAlgorithms();
+                txtResult.setText(itr.PrintAARCheck());
+            }
         });
 
     }
