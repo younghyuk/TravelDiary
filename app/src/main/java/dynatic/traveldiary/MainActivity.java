3adf976dbbff4f059e947cca3ef0f24c30975ba1
Kyora.Lee <kyora33@naver.com>
2017-08-30 23:39:43 +0900

    AAR

--- app/src/main/java/dynatic/traveldiary/MainActivity.java
+++ app/src/main/java/dynatic/traveldiary/MainActivity.java
@@ -2,13 +2,31 @@ package dynatic.traveldiary;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import kyora.itineraryalgorithms.ItineraryAlgorithms;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnCheck;
+    private TextView txtResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Init Test
+
+        btnCheck = (Button)findViewById(R.id.btnCheck);
+        txtResult = (TextView)findViewById(R.id.txtResult);
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                ItineraryAlgorithms itr = new ItineraryAlgorithms();
+                txtResult.setText(itr.PrintAARCheck());
+            }
+        });
     }
 }
---
+++ itineraryAlgorithms/src/main/java/kyora/itineraryalgorithms/ItineraryAlgorithms.java
@@ -0,0 +1,15 @@
+package kyora.itineraryalgorithms;
+
+import android.widget.Toast;
+
+/**
+ * Created by Kyora.Lee on 2017-08-29.
+ */
+
+public class ItineraryAlgorithms {
+    public String PrintAARCheck()
+    {
+        String result = "AAR Check";
+        return result;
+    }
+}
